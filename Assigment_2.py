855b5b99a3fb22907c4f6385ca987085797fb966
MD AJHARUDDIN <118420367+ajhark01@users.noreply.github.com>
2025-09-07 13:56:44 +0530

    Task 1: Check if a Number is Even or Odd

---
+++ Assigment_2.py
@@ -0,0 +1,10 @@
+# ASSIGNMENT 2:
+# Module 3: Control Structures in Python
+#  Task 1: Check if a Number is Even or Odd
+
+while True:
+    num_1=int(input('Enter a number: '))
+    if num_1//2:
+        print(num_1, "is an even number")
+    else:
+        print (num_1, "is not an Odd number")
